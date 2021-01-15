2213c0fdf8b4f1afb3998da582a6cfa3962e960f
artypineda <artypineda@gmail.com>
2021-01-15 13:02:54 -0800

    added tag manager to body too

--- pages/_document.js
+++ pages/_document.js
@@ -18,10 +18,28 @@ class MyDocument extends Document {
           ></script>
 
           <script src="/ga.js" async></script>
-          <script async src="/ga_tag.js"></script>
+
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5TSWMF2');`,
+            }}
+          ></script>
         </Head>
 
         <body>
+          <noscript
+            async
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TSWMF2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+
           <Main />
           <NextScript />
         </body>
